ac9e43478f0c6e9bab44762a03e3e09e4189bcee
Shubham-1196 <shubham6265171196@gmail.com>
2025-10-23 12:29:30 +0530

    Add all Java project files

---
+++ Product.java
@@ -0,0 +1,52 @@
+import java.io.Serializable;
+//import java.util.*;
+
+public class Product implements Serializable {
+    private int productId;
+    private String productName;
+    private int quantity;
+    private int threshold;
+
+    public Product(int productId, String productName, int quantity, int threshold) {
+        this.productId = productId;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.threshold = threshold;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public synchronized void increaseQuantity(int qty) {
+        quantity += qty;
+    }
+
+    public synchronized void decreaseQuantity(int qty) {
+        if (qty <= quantity) {
+            quantity -= qty;
+        } else {
+            System.out.println("Not enough stock available for " + productName);
+        }
+    }
+
+    public boolean isLowStock() {
+        return quantity <= threshold;
+    }
+
+    public String toString() {
+        return productId + " - " + productName + " | Qty: " + quantity + " | Threshold: " + threshold;
+    }
+}
